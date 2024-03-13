927c8d893f1da76c2fe027ccb2bd0e504825fd9f
wniazi5 <156951596+wniazi5@users.noreply.github.com>
2024-03-13 19:08:18 +0000

    done

--- main.hpp
+++ main.hpp
@@ -21,11 +21,25 @@ int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
 
 
 {
-        int mean = ((rdnum1 + rdnum2 + rdnum3) / 3)
+        int mean = ((rdnum1 + rdnum2 + rdnum3) / 3);
 
         int dif1 = abs( rdnum1 - mean);
-        int 
-        /***************************************************
-         * Code your program here
-         ***************************************************/
+        int dif2 = abs (rdnum2 - mean);
+        int dif3 = abs (rdnum3 - mean);
+
+        if ( dif1 <= dif2 && dif1 <= dif3){
+                return rdnum1;
+        }
+        else if (dif2 <= dif1 && dif2 <= dif3)
+        {
+        return rdnum2;
+        }
+        else {
+                return rdnum3;
+        }
+        
 }
+        int main (){
+                srand(time(0));
+                return (0);
+        }
